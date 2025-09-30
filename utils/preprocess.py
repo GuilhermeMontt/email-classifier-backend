74bc95f2e7718d991dcc57cbea74d6d5df6aed4e
GuilhermeMontt <guilhermaad8@gmail.com>
2025-09-29 23:55:09 -0300

    Tentando deploy

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -4,6 +4,10 @@ from nltk.corpus import stopwords
 import os
 
 # Adiciona o caminho para os dados do NLTK que serão baixados no build
+nltk_data_path = os.path.join(os.getcwd(), "nltk_data")
+if os.path.exists(nltk_data_path):
+    nltk.data.path.append(nltk_data_path)
+
 def clean_text(text: str) -> str:
     text = text.lower()
     text = re.sub(r"[^a-zA-ZÀ-ÿ0-9\s]", "", text)
